d02814aa2bb8ad17789e493d379ce61dda154e77
dadadadididada <45377710+dadadadididada@users.noreply.github.com>
2024-12-13 17:23:53 +0800

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
-echo "Please provide an element as an argument."
 
 if [[ $1 ]]
 then
-  # get columns that matches argument
-  if [[ SELECT IsNumeric($1) ]]
+  # check input's data type and funnel them into the correct condition
+  if [[ $1 =~ ^[0-9]+$ ]]
   then
-    echo its int!
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+  else
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1' OR name='$1'")
   fi
 
-  # select all properties
-  SELECT_ALL_PROPERTIES=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number=$1")
-  echo $SELECT_ALL_PROPERTIES
-  # read into variables
-  #echo $SELECT_ALL_PROPERTIES while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
-  
-  # output element properties
-  #echo -e "\nThe element with atomic number $"
+  # catch if input does not match data in DB
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
 
-  # if $1 doesn't match any data
+  else
+    # select all properties
+    SELECT_ALL_PROPERTIES=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+
+    # read into variables
+    echo $SELECT_ALL_PROPERTIES | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+    do
+      # output element properties
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      
+    done
+
+  fi
+
+else
   # display message
+  echo "Please provide an element as an argument."
+
 fi
